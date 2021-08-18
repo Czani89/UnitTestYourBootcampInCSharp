0a560c592a0de1e22b743995e1b55a15cd89df15
czani89 <czani89@gmail.com>
2021-08-18 08:55:23 +0200

    added solution file

---
+++ BootcampFunctions/Program.cs
@@ -0,0 +1,12 @@
+ using System;
+
+namespace BootcampFunctions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
